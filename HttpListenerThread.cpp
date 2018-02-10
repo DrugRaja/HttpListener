c5017e7312eda1defad16aab1275a0ac5173070b
msavanovic <milos83@gmail.com>
2018-02-10 13:59:37 +0100

    Add listener thread class implementation file

---
+++ HttpListenerThread.cpp
@@ -0,0 +1,15 @@
+﻿#include "HttpListenerThread.h"
+
+void HttpListenerThread::operator() (unsigned int port)
+{
+    printf("Listening at %d... \n", port);
+    if (!event_init())
+    {
+        printf("libevent error!\n");
+    }
+    else
+    {
+        printf("libevent success!\n");
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+}
